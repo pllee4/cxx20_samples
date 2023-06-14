0716e00c59ccf27c5c5888827a4535d569304ebc
Pin Loon Lee <pinloon_0428@hotmail.com>
2023-06-14 20:17:01 +0800

    build_and_run_using_docker.sh: updated to only run when build successful

--- build_and_run_using_docker.sh
+++ build_and_run_using_docker.sh
@@ -1,2 +1,6 @@
-sudo docker build --rm --force-rm . -t local/cxx20_samples:latest -f ./Dockerfile.cxx20_samples
-sudo docker run -it local/cxx20_samples:latest
+if sudo docker build --rm --force-rm . -t local/cxx20_samples:latest -f ./Dockerfile.cxx20_samples ; then
+    sudo docker run -it local/cxx20_samples:latest
+else
+    echo "Build error!"
+    exit 1
+fi
