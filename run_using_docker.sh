2abd83036503cf3c81bc8cfea6cfc83bb43c4dde
Pin Loon Lee <pinloon_0428@hotmail.com>
2024-12-01 22:39:24 +0800

    renamed and fixed attached volume for docker script

--- run_using_docker.sh
+++ run_using_docker.sh
@@ -1,6 +1,6 @@
 if docker build --rm --force-rm . -t local/cxx20_samples:latest -f ./Dockerfile.cxx20_samples ; then
     DIR=$(pwd)
-    docker run -it --volume="$DIR/src:/cxx20_samples/src" local/cxx20_samples:latest
+    docker run -it --volume="$DIR/src:/home/docker/ws/src" local/cxx20_samples:latest
 else
     echo "Build error!"
     exit 1
