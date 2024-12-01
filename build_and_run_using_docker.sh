25949ea0672757953da3021c64f717d0c1ecb819
Pin Loon Lee <pinloon_0428@hotmail.com>
2024-12-01 17:23:34 +0800

    update Dockerfile and script for easier development

--- build_and_run_using_docker.sh
+++ build_and_run_using_docker.sh
@@ -1,5 +1,6 @@
-if sudo docker build --rm --force-rm . -t local/cxx20_samples:latest -f ./Dockerfile.cxx20_samples ; then
-    sudo docker run -it local/cxx20_samples:latest
+if docker build --rm --force-rm . -t local/cxx20_samples:latest -f ./Dockerfile.cxx20_samples ; then
+    DIR=$(pwd)
+    docker run -it --volume="$DIR/src:/cxx20_samples/src" local/cxx20_samples:latest
 else
     echo "Build error!"
     exit 1
