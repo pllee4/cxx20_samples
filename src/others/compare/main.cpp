be80c2ec6c8b0126555532f8e8f13bc795149058
Pin Loon Lee <pinloon_0428@hotmail.com>
2025-08-29 19:58:33 +0800

    src: others: compare: add more relational functions

--- src/others/compare/main.cpp
+++ src/others/compare/main.cpp
@@ -21,4 +21,10 @@ int main() {
   } else if (compare > 0) {
     std::cout << "a > b\n";
   }
+  std::cout << "Equal " << std::is_eq(a <=> b) << std::endl;
+  std::cout << "Not Equal " << std::is_neq(a <=> b) << std::endl;
+  std::cout << "Less Than " << std::is_lt(a <=> b) << std::endl;
+  std::cout << "Greater Than " << std::is_gt(a <=> b) << std::endl;
+  std::cout << "Less Than or Equal " << std::is_lteq(a <=> b) << std::endl;
+  std::cout << "Greater Than or Equal " << std::is_gteq(a <=> b) << std::endl;
 }
